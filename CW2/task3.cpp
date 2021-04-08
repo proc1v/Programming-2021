0abf5ccf4a88e25b87f6fb3e52cfca490f99ce49
proc1v <71555163+proc1v@users.noreply.github.com>
2021-04-08 13:26:13 +0300

    Add files via upload

---
+++ CW2/task3.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+
+void readArr(int arr[], int size)
+{
+	for (int i = 0; i < size; ++i) {
+		cout << "arr[" << i << "] = ";
+		cin >> arr[i];
+	}
+}
+
+void printArr(int arr[], int size)
+{
+	cout << endl;
+	for (int i = 0; i < size; ++i)
+		cout << arr[i] << ' ';
+	cout << endl;
+}
+
+void delete_identical_elements(int arr1[], int arr2[], int& size1, int& size2)
+{
+	int count = 0;
+	bool identical = false;
+	int* temp_arr = new int[size1];
+	
+	for (int i = 0; i < size1; ++i) {
+		for (int j = 0; j < size2; ++j) {
+			if (arr1[i] == arr2[j]) {
+				identical = true;
+				count++;
+				break;
+			}
+		}
+		if (!identical)
+			temp_arr[i] = arr1[i];
+	}
+
+	size1 -= count;
+	int* new_arr1 = new int[size1];
+	for (int i = 0; i < size1; ++i) {
+		new_arr1[i] = temp_arr[i];
+	}
+	delete[] temp_arr;
+	arr1 = new_arr1;
+}
+
+void task3()
+{
+	int size1, size2;
+	
+	cout << "enter size of 1st array:";
+	cin >> size1;
+	int* arr1 = new int[size1];
+	readArr(arr1, size1);
+
+	cout << "enter size of 2nd array:";
+	cin >> size2;
+	int* arr2 = new int[size2];
+	readArr(arr2, size2);
+
+	cout << "1st array:";
+	printArr(arr1, size1);
+	cout << "2nd array:";
+	printArr(arr2, size2);
+
+	delete_identical_elements(arr1, arr2, size1, size2);
+
+	cout << "Changed 1st array:";
+	printArr(arr1, size1);
+
+	delete[] arr1;
+	delete[] arr2;
+
+}
+
+int main()
+{
+	task3();
+}
